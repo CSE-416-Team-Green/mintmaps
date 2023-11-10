6967b11e91321fd01bdcd3c85c5a040760a50033
araguma <itetbitb@gmail.com>
2023-11-09 22:53:19 -0500

    feat: Implement Header component

---
+++ pages/map-creation.tsx
@@ -0,0 +1,15 @@
+import Header from '@/components/Header';
+
+const steps = [
+    'Upload/Choose preset',
+    'Map Type',
+    'Description',
+]
+
+const MapCreation = () => {
+    return (
+        <Header />
+    )
+}
+
+export default MapCreation;
