0b155dbbadaf591fc0ab6716fc1cbd1a8f32a0e5
Ramy <ramy@Kurama>
2023-11-11 05:17:06 -0500

    feat:added tests for auth routes

---
+++ __tests__/isLoggedIn.test.ts
@@ -0,0 +1,11 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import server from "nextjs-http-supertest";
+import request from "supertest";
+import { expect, jest, test } from "@jest/globals";
+
+describe("login with Google API Endpoint", () => {
+    it("should return 405 Method Not Allowed when using any non-GET method", async () => {
+        const result = await request(server).post("/api/isLoggedIn");
+        expect(result.status).toBe(405);
+    });
+});
