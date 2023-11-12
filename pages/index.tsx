a64f531a25b465fd6ec4a1b72961f841b4c664e5
Ramy <ramy@Kurama>
2023-11-12 16:13:15 -0500

    feat :dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,6 @@
 import Home from "@/pages/home";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Login from "./login";
+import * as React from "react";
 
 export default function App() {
-    const theme = createTheme({
-        palette: {
-            primary: {
-                main: "#2ecc71",
-            },
-        },
-    });
-
-    return (
-        <ThemeProvider theme={theme}>
-            <Home/>
-        </ThemeProvider>
-    );
+    return <Home />;
 }
