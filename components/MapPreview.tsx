283934e95fb03515457a85bb94d37af52dd90738
nick <nicholas.aloi@stonybrook.edu>
2023-11-12 16:59:39 -0500

    set up paths to all screens

--- components/MapPreview.tsx
+++ components/MapPreview.tsx
@@ -8,9 +8,14 @@ import { IconButton, Typography } from '@mui/material';
 import { green } from '@mui/material/colors';
 
 const MapPreview = () => {
+
+    function handleClick() {
+        console.log("click");
+    }
+
     return (
         <div >
-            <Grid container sx={{width:300, height:230}} justifyContent="left" alignItems={"center"} spacing={0.5} rowSpacing={0.2}>
+            <Grid container onClick={handleClick} sx={{width:300, height:250}} justifyContent="left" alignItems={"center"} spacing={0.5} rowSpacing={0.2} paddingBottom={5}>
                 <Grid item xs={12}>
                     <Container component={"img"} src={"/heatmap-preview.png"} sx={{width:300, height:150, float:'left'}}></Container>
                 </Grid>
@@ -22,17 +27,25 @@ const MapPreview = () => {
                 <Grid item xs={8}> 
                     <Grid container justifyContent="left" alignItems={"center"}>
                         <Grid item xs={11}>
-                            <Typography sx={{fontSize:'20px'}}>
-                                Map Title
-                            </Typography>
+                                <Link
+                                    href="/map-info"
+                                >
+                                    <Typography sx={{fontSize:'20px', color:'black'}}>
+                                        Map Title
+                                    </Typography>
+                                </Link>
                         </Grid>
                         <Grid item xs={1}>
                             <MoreVertIcon />
                         </Grid>
                         <Grid item xs={6}>
-                            <Typography sx={{fontSize:'13px'}}>
-                                Username
-                            </Typography>
+                            <Link
+                                    href="/user-profile"
+                                >
+                                    <Typography sx={{fontSize:'13px', color: 'black'}}>
+                                        Username
+                                    </Typography>
+                            </Link>
                         </Grid>
                         <Grid item xs={6}>
                             <Typography sx={{float:'right', fontSize:'13px'}}>
