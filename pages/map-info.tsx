8519417403e879762ba6eb69329704b843d3e90c
nick <nicholas.aloi@stonybrook.edu>
2023-11-13 04:44:30 -0500

    actual nice mapinfo layout

--- pages/map-info.tsx
+++ pages/map-info.tsx
@@ -17,61 +17,70 @@ export default function MapInfo() {
             <Header />
             <br /> 
             <Grid 
-                container
-                direction={"row"}
-                sx={{ width: "100%", height: "100%" }}
-                justifyContent="left"
-                alignItems={"left"}
+                    container
+                    direction={"row"}
+                    sx={{ width: "100%", height: "100%"}}
+                    justifyContent="left"
+                    alignItems={"left"}
             >
-                <Grid item xs={1} />
-                <Grid item xs={8.5}>
-                    <Box sx={{
-                        height: "70vh",
-                        width: "70vw",
-                        display: "flex",
-                    }}>
-                        <DynamicMap />
-                    </Box>
-                </Grid>
-                <Grid item xs={2.5} />
+                <Grid item xs={9}>
+                    <Grid
+                        container
+                        direction={"row"}
+                        sx={{ width: "95%", height: "100%", position:"relative", left:"5%" }}
+                        justifyContent="left"
+                        alignItems={"left"}
+                    >
+                        <Grid item xs={12}>
+                            <Box sx={{
+                                height: "70vh",
+                                width: "70vw",
+                                display: "flex",
+                            }}>
+                                <DynamicMap />
+                            </Box>
+                        </Grid>
 
-                <Grid item xs={1} />
-                <Grid item xs={6}>
-                    Title
-                </Grid>
-                <Grid item xs={1}>
-                    200 Views
-                </Grid>
-                <Grid item xs={1.5}>
-                    Uploaded 2 Weeks Ago
-                </Grid>
-                <Grid item xs={2.5} />
-                
-                <Grid item xs={1} />
-                <Grid item xs={0.5}>
-                    pfp
-                </Grid>
-                <Grid item xs={5}>
-                    name,followers,follow button
-                </Grid>
-                <Grid item xs={1}>
-                    likes,dislikes
-                </Grid>
-                <Grid item xs={2}>
-                    social buttons
-                </Grid>
-                <Grid item xs={2.5} />
+                        <Grid item xs={9}>
+                            Title
+                        </Grid>
+                        <Grid item xs={1}>
+                            200 Views
+                        </Grid>
+                        <Grid item xs={2}>
+                            Uploaded 2 Weeks Ago
+                        </Grid>
+                        
+                        <Grid item xs={0.5}>
+                            pfp
+                        </Grid>
+                        <Grid item xs={8}>
+                            name,followers,follow button
+                        </Grid>
+                        <Grid item xs={1.5}>
+                            likes,dislikes
+                        </Grid>
+                        <Grid item xs={2}>
+                            social buttons
+                        </Grid>
+                        <Grid item xs={2.5} />
 
-                <Grid item xs={1} />
-                <Grid item xs={1}>
-                    description
+                        <Grid item xs={12}>
+                            description
+                        </Grid>
+                    </Grid>
                 </Grid>
-                <Grid item xs={2.5} />
-
-            </Grid>
-
-                    
-
+                <Grid item xs={3}>
+                        <Box sx={{
+                                    display: "flex",
+                                    position: "relative",
+                                    backgroundColor: "black",
+                                    width:"20vw"
+                                }}>
+                                    tr
+                        </Box>
+                    </Grid>   
+            </Grid> 
         </>
     );
 }
