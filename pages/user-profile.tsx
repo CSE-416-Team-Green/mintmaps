bdf3be222da3d55fcea92e9333c4b0a6f194f549
nick <nicholas.aloi@stonybrook.edu>
2023-11-12 19:13:23 -0500

    profile screen layout kinda

--- pages/user-profile.tsx
+++ pages/user-profile.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import MapPreview from "@/components/MapPreview";
 import LogoLarge from "@/components/LogoLarge";
+import Avatar from '@mui/material/Avatar';
 import * as React from "react";
 import { Container, Grid, Typography } from "@mui/material";
 import styles from '@/styles/about.module.css';
@@ -20,6 +21,52 @@ export default function UserProfile() {
                     <Header />
                 </Grid>
                 <Container>
+                    <br></br>
+                    <Grid 
+                        container
+                        direction={"row"}
+                        sx={{ width: "100%", height: "100%" }}
+                        justifyContent="left"
+                        alignItems={"left"}
+                    >
+                        <Grid item xs={3}>
+                            <Avatar sx={{height:'250px', width:'250px'}}/>  
+                        </Grid>
+                        <Grid item xs={9}>
+                            <Grid 
+                                container
+                                direction={"row"}
+                                justifyContent="left"
+                                alignItems={"left"}
+                                rowSpacing={1.5}
+                            >
+                                <Grid item xs={3}>
+                                    <div className={styles.usernameText}>
+                                        Username
+                                    </div>
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <div className={styles.usernameText}>
+                                        Follow 
+                                    </div>
+                                </Grid>
+                                <Grid item xs={3}>
+                                    <div className={styles.usernameText}>
+                                        Share
+                                    </div>
+                                </Grid>
+                                <Grid item xs={2}>3 Followers</Grid>
+                                <Grid item xs={2}>3 Following</Grid>
+                                <Grid item xs={2}> Reputation</Grid>
+                                <Grid item xs={6}></Grid>
+                                <Grid item xs={12}>
+                                    <Typography className={styles.descriptionText}>
+                                        ASDASDASDASDASDASDASDASDASDASDASDAKJHGVSDFJHSDFVHJBSDJVHBFBVJ SFDJHVLSDFJHVLKSDFVHJSFDVJHJVHFSDJHKVFSD
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                     <div className={styles.homeText}>
                         User Profile
                     </div>
