621201e3ea9ece5214d803ca1f0756f37f012096
nick <nicholas.aloi@stonybrook.edu>
2023-11-13 10:34:45 -0500

    user profile done

--- pages/user-profile.tsx
+++ pages/user-profile.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { Button } from "@mui/material";
 import ShareIcon from '@mui/icons-material/Share';
 import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import SortIcon from '@mui/icons-material/Sort';
 
 export default function UserProfile() {
     return (
@@ -70,35 +71,55 @@ export default function UserProfile() {
                                 </Grid>
                             </Grid>
                         </Grid>
-                    </Grid>
-                    <div className={styles.homeText}>
-                        User Profile
-                    </div>
-                    <div className={styles.homeBox}>
-                        <Grid container direction={"row"} alignItems={"left"} justifyContent={"left"} >
-                            <Grid item xs={3}>
-                                <MapPreview />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <MapPreview />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <MapPreview />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <MapPreview />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <MapPreview />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <MapPreview />
-                            </Grid>
-                            <Grid item xs={3}>
-                                <MapPreview />
+
+                        
+                        <Grid item xs={3} sx={{paddingTop:"40px"}}>
+                            <Grid 
+                                container
+                                direction={"column"}
+                                sx={{ width: "100%", height: "100%", backgroundColor:"#f1f1f1"}}
+                                justifyContent="center"
+                                alignItems={"center"}
+                            >
+                                <Grid item xs={0.5} sx={{paddingTop:"30px", fontSize:"25px"}}>User's Maps </Grid>
+                                <Grid item xs={0.5} sx={{paddingTop:"30px", fontSize:"25px"}}>Liked Maps</Grid>
+                                <Grid item xs={0.5} sx={{paddingTop:"30px", fontSize:"25px"}}>Saved Maps </Grid>
+                                <Grid item xs={0.5} sx={{paddingTop:"30px", fontSize:"25px"}}>Following </Grid>
+                                <Grid item xs={0.5} sx={{paddingTop:"30px", paddingBottom:"250px", fontSize:"25px"}}>Followers </Grid>
                             </Grid>
                         </Grid>
-                    </div>
+
+                        <Grid item xs={9} sx={{paddingTop:"40px"}}>
+                            <Grid item xs={12} sx={{fontSize: "25px", paddingBottom: "10px", paddingTop: "4px", paddingLeft: "10px"}}>
+                                <SortIcon /> Sort By
+                            </Grid>
+                            <div className={styles.homeBox}>
+                                <Grid container direction={"row"} alignItems={"left"} justifyContent={"left"} >
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                    <Grid item xs={3}>
+                                        <MapPreview />
+                                    </Grid>
+                                </Grid>
+                            </div>
+                        </Grid>
+                    </Grid>
                 </Container>
                 
             </Grid>
