a35baa9e20cb550c6566ec2a529829648bffa75a
Ramy <ramy@Kurama>
2023-11-20 10:56:16 -0500

    fix: fixed test

--- cypress/e2e/login-signup-tests/login.cy.ts
+++ cypress/e2e/login-signup-tests/login.cy.ts
@@ -3,13 +3,6 @@ describe("Test Login Page and Login Modal", () => {
         cy.visit("http://localhost:3000/login");
     });
 
-    it("Useranme field is able to receive test", () => {
-        cy.contains("Username")
-            .parent()
-            .find("input")
-            .type("users")
-            .should("have.value", "users");
-    });
 
     it("Password field is able to receive test", () => {
         cy.contains("Password")
