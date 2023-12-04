309c40524d905056fbdbdff8c4e404361721cc1a
nick <nicholas.aloi@stonybrook.edu>
2023-12-04 02:21:52 -0500

    get all maps

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,14 +1,45 @@
 import { TextField, IconButton, Icon } from '@mui/material';
 import InputAdornment from '@mui/material/InputAdornment';
 import SearchIcon from '@mui/icons-material/Search';
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+    const router = useRouter();
+    const handleClick = async () => {
+
+        const payload = {
+            type: "",
+            filter: "",
+            sort: "",
+            searchTerm: ""
+        }
+
+
+        try {
+            const response = await fetch('/api/searchMaps', {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload),
+            });
+    
+            if (response.ok) {
+                console.log(response.body);
+                router.push("/search-results");
+                console.log(response);
+            } else {
+
+            }
+        }catch (error) {
+            console.error('Error performing search:', error);
+            alert('An error occurred while performing your search.');
+        }
+    }
     return (
         <TextField
             size='small'
             InputProps={{
                 startAdornment: <InputAdornment position="start">
-                    <IconButton href='search-results'> 
+                    <IconButton onClick={handleClick}> 
                         <SearchIcon />
                     </IconButton>
                     
--- pages/api/searchMaps.ts
+++ pages/api/searchMaps.ts
@@ -1,11 +1,44 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import connectDb from "@/db";
+import Map from "@/models/Map";
 
-export default function handler(
+export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-    if(req.method !== "GET") {
+    if(req.method !== "PUT") {
         res.status(401).json({message: "Method not allowed"})
         return
     }
+
+    try {
+        await connectDb();
+    } catch (err) {
+        console.error("Error connecting to the database:", err);
+        return res
+            .status(500)
+            .json({ message: "error connecting to database" });
+    }
+
+    const { type, filter, sort, searchTerm } = req.body;
+
+    //get all maps
+    const allMaps = await Map.find({}, {geoJSON:0, legend:0, })
+    console.log(allMaps);
+
+    //filter by desired map type
+
+
+    //filter by all, title, uploader, or tags
+
+
+    //sort by parameter
+
+
+    //return array of maps to be used in search results
+    //maybe only want to return certain info? maybe even just mapIds?
+
+    const maps = [Map];
+
+    return res.status(200).json({headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(allMaps) });
 }
