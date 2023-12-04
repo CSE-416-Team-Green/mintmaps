90d4766d466129a74368d50afed9956bfa88f6f2
Ramy <ramy@Kurama>
2023-12-04 09:02:20 -0500

    feat: commenting on map

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -1,8 +1,17 @@
-import { Container, Grid, Paper } from "@mui/material";
+import FormatDateText from "@/utils/dateTextUtils";
+import {
+    Container,
+    Grid,
+    Paper,
+    IconButton,
+    Typography,
+    Stack,
+} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 
 interface commentProps {
     comment: {
-        user: string;
+        user: any;
         message: string;
         uploadDate: Date;
         replies: string[];
@@ -14,11 +23,40 @@ interface commentProps {
 const Comment: React.FC<commentProps> = ({ comment }) => {
     return (
         <Container>
-            <Paper elevation={3}>
-                <Grid direction={"row"}>
-                    <Grid item></Grid>
+            <Paper elevation={3} sx={{ height: "100%" }}>
+                <Grid container direction={"column"}>
+                    <Grid item>
+                        <Grid container direction="row">
+                            <Grid item sx={{ mt: 1 }}>
+                                <IconButton href="/user-profile">
+                                    <Avatar />
+                                </IconButton>
+                            </Grid>
+                            <Grid item sx={{ mt: 2, ml: 5, minWidth: 250 }}>
+                                <Typography variant="h5">
+                                    {comment.message}
+                                </Typography>
+                            </Grid>
+
+                            <Grid item sx={{ mt: 2, ml: 55, mb: 1 }}>
+                                <Stack direction={"column"}>
+                                    <Typography variant="body1">
+                                        {comment.user.userName}
+                                    </Typography>
+                                    <Typography variant="body1">
+                                        {FormatDateText.formatDateText(
+                                            comment.uploadDate,
+                                            ""
+                                        )}
+                                    </Typography>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Paper>
         </Container>
     );
 };
+
+export default Comment;
---
+++ pages/api/addComment.ts
@@ -0,0 +1,66 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Comment from "@/models/Comment";
+import connectDb from "@/db";
+import MapModel from "@/models/Map";
+import User from "@/models/User";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+) {
+    if (req.method !== "POST") {
+        res.status(401).json({ message: "Method not allowed" });
+    }
+
+    try {
+        connectDb();
+    } catch (error) {
+        console.error("Cant connect to database", error);
+    }
+
+    try {
+        const { user, message, mapId } = JSON.parse(req.body);
+
+        const commenter = await User.findOne({ email: user });
+        console.log(commenter._id);
+        console.log(message);
+        const newComment = new Comment({
+            user: commenter._id,
+            message: message,
+            uploadDate: Date.now(),
+        });
+        await newComment.save();
+
+        const map = await MapModel.findById(mapId).populate("comments").exec();
+
+        if (!map) {
+            res.status(404).json({ message: "Map not found" });
+            return;
+        }
+
+        map.comments.push(newComment._id);
+        await map.save();
+
+        const updatedMap = await MapModel.findById(mapId)
+            .populate({
+                path: "comments", 
+                populate: {
+                    path: "user",
+                    model: "User", 
+                },
+            })
+            .exec();
+
+        return res.status(200).json({
+            message: "Comment added successfully",
+            newComments: updatedMap.comments.sort(
+                (a: any, b: any) => b.uploadDate - a.uploadDate
+            ),
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error creating comment",
+        });
+        console.error("Error creating comment", error);
+    }
+}
