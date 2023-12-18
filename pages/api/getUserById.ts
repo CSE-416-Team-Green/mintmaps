3c7ba05a459f9611a4ae9670082204c692c58a10
keli chen <90227843+chavez777@users.noreply.github.com>
2023-12-17 23:08:21 -0500

    1

--- pages/api/getUserById.ts
+++ pages/api/getUserById.ts
@@ -18,9 +18,8 @@ export default async function handler(
   await connectDb();
   try {
     const email = req.query.email as string;
-    
-    
     // console.log(email);
+
     await connectDb();
 
     const user = await User.findOne({ email: email });
