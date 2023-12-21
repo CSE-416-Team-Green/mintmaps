58d27acc9b603fbfdac11224a004030665857f58
Ramy <ramy@Kurama>
2023-12-21 01:26:29 -0500

    feat: all tests passed

--- cypress/e2e/admin-page-tests/admin_panel_basics.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Admin Panel should be interactive", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:3000/usermanagement");
-    });
-
-    it("New Button clickable", () => {
-        cy.contains("New").click();
-    });
-});
